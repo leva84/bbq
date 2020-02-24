511b14b4ab3189d3039191e65f1dff89df086a6d
leva84 <denfrut8@gmail.com>
2020-02-24 13:05:59 +0300

    Views devaise and model User update

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,12 @@ class User < ApplicationRecord
   # Добавим заодно валидации для юзера
   # Имя не не более 35 символов
   validates :name, presence: true, length: {maximum: 35}
-  # Уникальный email по заданному шаблону не более 255
-  # символов
-  validates :email, presence: true, length: {maximum: 255}
+
+  before_validation :set_name, on: :create
+
+  private
+
+  def set_name
+    self.name = "Товарисч №#{rand(777)}" if self.name.blank?
+  end
 end
