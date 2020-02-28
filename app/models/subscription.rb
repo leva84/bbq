ba4143d17c18b05d0b024008294dfe0ebbdff73e
leva84 <denfrut8@gmail.com>
2020-02-28 16:38:50 +0300

    Add validate :subscription_owner, if: -> { user.present? }

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -16,7 +16,9 @@ class Subscription < ApplicationRecord
   validates :user, uniqueness: { scope: :event_id }, if: -> { user.present? }
   # Или один email может использоваться только один раз (если анонимная подписка)
   validates :user_email, uniqueness: { scope: :event_id }, unless: -> { user.present? }
+
   validate :email_is_free, unless: -> { user.present? }
+  validate :subscription_owner, if: -> { user.present? }
 
   # Если есть юзер, выдаем его имя,
   # если нет – дергаем исходный метод
@@ -44,5 +46,11 @@ class Subscription < ApplicationRecord
     return unless User.find_by_email(user_email)
 
     errors.add(:user_email, 'ты охуел чтоли?!' )
+  end
+
+  def subscription_owner
+    return unless create_user.id == Event.find_by_user_id(user_id)
+
+    errors.add(:user_id, 'Идиот, это же твое событие, да?!')
   end
 end
