aa8ab986d9b858565431c5cafe318a85f50cbd5f
leva84 <denfrut8@gmail.com>
2020-03-01 12:53:54 +0300

    Add transfer in yml.ru for validete email_is_free and subscription_owner for model subscription

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -43,10 +43,12 @@ class Subscription < ApplicationRecord
   private
 
   def email_is_free
-    errors.add(:user_email, 'уже зарегестрирован такой пользователь') if User.find_by(email: user_email)
+    errors.add(:user_email, (I18n.t'activerecord.attributes.subscription.email_is_free.user_email')) if
+        User.find_by(email: user_email)
   end
 
   def subscription_owner
-    errors.add(:user, 'Это твое событие, ты и так пойдешь)') if user.id == event.user_id_was
+    errors.add(:user_id, (I18n.t'activerecord.attributes.subscription.subscription_owner.user_id')) if
+        user.id == event.user_id_was
   end
 end
