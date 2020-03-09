5c5d4349bcfcbc3e65f2bf78cbaf5c611f4cd87c
leva84 <denfrut8@gmail.com>
2020-03-09 22:51:45 +0300

    'Add fog_region = bbq-1.s3.amazonaws.com' in carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,7 @@ require 'carrierwave/storage/fog'
 if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_provider = 'fog/aws'
-    config.fog_region = ENV[1635]
+    config.fog_region = ENV['bbq-1.s3.amazonaws.com']
     config.fog_credentials = {
         provider:              'AWS',
         aws_access_key_id:     ENV['S3_ACCESS_KEY'],
