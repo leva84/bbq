6d10d6aa0b4d16a861df69eaf4f2f5584adc26b5
leva84 <denfrut8@gmail.com>
2020-03-10 11:16:34 +0300

    'Add ENV['bbq-1.s3.eu-north-1.amazonaws.com'] in carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,7 +7,7 @@ if Rails.env.production?
         provider:              'AWS',
         aws_access_key_id:      ENV['S3_ACCESS_KEY'],
         aws_secret_access_key:  ENV['S3_SECRET_KEY'],
-        fog_region:             ENV['bbq-1.s3.eu-north-1.amazonaws.com']
+        fog_region:             ENV['bbq-1.s3.eu-north-1']
     }
 
     config.fog_directory  = ENV['S3_BUCKET_NAME']
