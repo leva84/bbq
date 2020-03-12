d9994fc011cc5f25a8b9517d5a35ef646035a92c
leva84 <denfrut8@gmail.com>
2020-03-12 14:35:25 +0300

    Add event mailer for commit and subscription. Devise_mailer for  reset_password

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,16 +6,15 @@ class CommentsController < ApplicationController
 
   # POST /comments
   def create
-    # Создаём объект @new_comment из @event
     @new_comment = @event.comments.build(comment_params)
-    # Проставляем пользователя, если он задан
     @new_comment.user = current_user
 
     if @new_comment.save
-      # Если сохранился, редирект на страницу самого события
+      # уведомляем всех подписчиков о новом комментарии
+      notify_subscribers(@event, @new_comment)
+
       redirect_to @event, notice: I18n.t('controllers.comments.created')
     else
-      # Если ошибки — рендерим здесь же шаблон события (своих шаблонов у коммента нет)
       render 'events/show', alert: I18n.t('controllers.comments.error')
     end
   end
@@ -47,5 +46,17 @@ class CommentsController < ApplicationController
 
   def comment_params
     params.require(:comment).permit(:body, :user_name)
+  end
+
+  def notify_subscribers(event, comment)
+    # Собираем всех подписчиков и автора события в массив мэйлов, исключаем повторяющиеся
+    all_emails = (event.subscriptions.map(&:user_email) + [event.user.email]).uniq
+
+    # По адресам из этого массива делаем рассылку
+    # Как и в подписках, берём EventMailer и его метод comment с параметрами
+    # И отсылаем в том же потоке
+    all_emails.each do |mail|
+      EventMailer.comment(event, comment, mail).deliver_now
+    end
   end
 end
